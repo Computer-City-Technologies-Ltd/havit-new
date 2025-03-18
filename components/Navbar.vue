9382697a43715811639b9ecac14d99c3d1377118
alin-comcity <1000957@daffodil.ac>
2025-03-18 16:34:30 +0600

    api call and product show

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,73 +1,85 @@
 <template>
-    <nav class="navbar justify-between gap-4 shadow">
-        <!-- site logo start here -->
-        <div class="navbar-start">
-            <NuxtLink to="/">
-                <img src="/public/img/logo.png" class="w-40" />
-            </NuxtLink>
-        </div>
-        <!-- site logo end here -->
+  <nav class="navbar justify-between gap-4 shadow">
+    <!-- site logo start here -->
+    <div class="navbar-start">
+      <NuxtLink to="/">
+        <img src="/public/img/logo.png" class="w-40" />
+      </NuxtLink>
+    </div>
+    <!-- site logo end here -->
 
-        <!-- navigation menu start here -->
-        <div class="navbar-center flex items-center text-lg">
-            <div class="col-span-6 uppercase">
-                <ul class="flex">
-                    <NuxtLink to="/hakii">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Hakii</li>
-                    </NuxtLink>
-                    <NuxtLink to="/music">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Music</li>
-                    </NuxtLink>
-                    <NuxtLink to="/game">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Game</li>
-                    </NuxtLink>
-                    <NuxtLink to="/watch">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Watch</li>
-                    </NuxtLink>
-                    <NuxtLink to="/mobile">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
-                        Mobile
-                    </li>
-                    </NuxtLink>
-                    <NuxtLink to="/demo">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
-                        Demo
-                    </li>
-                    </NuxtLink>
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white dropdown relative inline-flex [--auto-close:inside] [--offset:9] [--placement:bottom-end]">
-                        <button id="dropdown-nav" type="button" class="uppercase dropdown-toggle dropdown-open:bg-base-content/10 dropdown-open:text-base-content" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown" >
-                            Products
-                        <span class="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"></span>
-                        </button>
-                        <ul class="dropdown-menu dropdown-open:opacity-100 hidden" role="menu" aria-orientation="vertical" aria-labelledby="dropdown-nav" >
-                            <li><a class="dropdown-item" href="#">UI kits</a></li>
-                            <li><a class="dropdown-item" href="#">Templates</a></li>
-                            <li><a class="dropdown-item" href="#">Component library</a></li>
-                            <hr class="border-base-content/25 -mx-2 my-3" />
-                            <li><a class="dropdown-item" href="#">Figma designs</a></li>
-                        </ul>
-                    </li>
-                    <NuxtLink to="/about">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
-                        About Us
-                    </li>
-                    </NuxtLink>
-                    <NuxtLink to="/contact">
-                    <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
-                        Contact Us
-                    </li>
-                    </NuxtLink>
-                </ul>
-            </div>
-        </div>
-        <!-- navigation menu end here -->
-
-        <!-- search start here -->
-        <div class="navbar-end items-center gap-4 text-lg">
-            <button class="btn btn-sm btn-text btn-circle size-[2.125rem]" aria-label="Search Button">
-            <span class="icon-[tabler--search] size-[1.375rem]"></span>
+    <!-- navigation menu start here -->
+    <div class="navbar-center flex items-center text-lg">
+      <div class="col-span-6 uppercase">
+        <ul class="flex">
+          <NuxtLink to="/hakii">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Hakii</li>
+          </NuxtLink>
+          <NuxtLink to="/music">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Music</li>
+          </NuxtLink>
+          <NuxtLink to="/game">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Game</li>
+          </NuxtLink>
+          <NuxtLink to="/watch">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Watch</li>
+          </NuxtLink>
+          <NuxtLink to="/mobile">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">Mobile</li>
+          </NuxtLink>
+          <li
+            class="px-4 py-1 hover:bg-gray-800 hover:text-white dropdown relative inline-flex [--auto-close:inside] [--offset:9] [--placement:bottom-end]"
+          >
+            <button
+              id="dropdown-nav"
+              type="button"
+              class="uppercase dropdown-toggle dropdown-open:bg-base-content/10 dropdown-open:text-base-content"
+              aria-haspopup="menu"
+              aria-expanded="false"
+              aria-label="Dropdown"
+            >
+              Products
+              <span
+                class="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"
+              ></span>
             </button>
-        </div>
-        <!-- search end here -->
-    </nav> 
+            <ul
+              class="dropdown-menu dropdown-open:opacity-100 hidden"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="dropdown-nav"
+            >
+              <li><a class="dropdown-item" href="#">UI kits</a></li>
+              <li><a class="dropdown-item" href="#">Templates</a></li>
+              <li><a class="dropdown-item" href="#">Component library</a></li>
+              <hr class="border-base-content/25 -mx-2 my-3" />
+              <li><a class="dropdown-item" href="#">Figma designs</a></li>
+            </ul>
+          </li>
+          <NuxtLink to="/about">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
+              About Us
+            </li>
+          </NuxtLink>
+          <NuxtLink to="/contact">
+            <li class="px-4 py-1 hover:bg-gray-800 hover:text-white">
+              Contact Us
+            </li>
+          </NuxtLink>
+        </ul>
+      </div>
+    </div>
+    <!-- navigation menu end here -->
+
+    <!-- search start here -->
+    <div class="navbar-end items-center gap-4 text-lg">
+      <button
+        class="btn btn-sm btn-text btn-circle size-[2.125rem]"
+        aria-label="Search Button"
+      >
+        <span class="icon-[tabler--search] size-[1.375rem]"></span>
+      </button>
+    </div>
+    <!-- search end here -->
+  </nav>
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,44 @@
 <template>
-
   <div class="w-full h-screen relative">
-    <img src="/img/aboutusbanner.jpg" class="absolute top-0 left-0 min-h-full ob" alt="">
+    <img
+      src="/img/aboutusbanner.jpg"
+      class="absolute top-0 left-0 min-h-full ob"
+      alt=""
+    />
   </div>
 
-  <div class="relative overflow-hidden bg-white">
+  <div class="relative overflow-hidden">
     <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
       <div class="flex flex-col items-end pr-12 pt-8">
         <div class="h-full mt-auto overflow-hidden relative">
-          <img src="/img/hq.jpg" alt="">
+          <img src="/img/hq.jpg" alt="" />
         </div>
       </div>
 
-      <div class="py-20 bg-gray-900 relative before:absolute before:h-full before:w-screen before:bg-gray-900 before:top-0 before:left-0">
+      <div
+        class="py-20 bg-gray-900 relative before:absolute before:h-full before:w-screen before:bg-gray-900 before:top-0 before:left-0"
+      >
         <div class="relative z-20 pl-12">
-          <h2 class="text-[#da3a2a] font-black text-5xl leading-snug mb-10">About Us</h2>
+          <h2 class="text-[#da3a2a] font-black text-5xl leading-snug mb-10">
+            About Us
+          </h2>
           <p class="text-white text-sm">
-            Authorized Distributor- COMPUTER CITY TECHNOLOGIES LTD Computer City Technologies Ltd is one of the largest ICT hardware distribution company as well as a solution provider in network and security space in Bangladesh. CCTL has been operating in Bangladesh ICT industry since 1999. Responding to the ICT needs of the country, CCTL was established with the goal to keep our generation updated with latest technology while assisting to secure the fastest access to the digital world. We have been able to set an example of excellence to our customers using the state of the art technology, quality products and dedicated pre-sales and post-sales support and service. 
+            Authorized Distributor- COMPUTER CITY TECHNOLOGIES LTD Computer City
+            Technologies Ltd is one of the largest ICT hardware distribution
+            company as well as a solution provider in network and security space
+            in Bangladesh. CCTL has been operating in Bangladesh ICT industry
+            since 1999. Responding to the ICT needs of the country, CCTL was
+            established with the goal to keep our generation updated with latest
+            technology while assisting to secure the fastest access to the
+            digital world. We have been able to set an example of excellence to
+            our customers using the state of the art technology, quality
+            products and dedicated pre-sales and post-sales support and service.
           </p>
-          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white bg-[#da3a2a] hover:bg-white hover:bg-opacity-10">Contact Us</button>
+          <button
+            class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white bg-[#da3a2a] hover:bg-white hover:bg-opacity-10"
+          >
+            Contact Us
+          </button>
         </div>
       </div>
     </div>
@@ -26,33 +46,71 @@
 
   <div class="py-4 relative overflow-hidden bg-white">
     <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-      
-      <div class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#da3a2a] before:top-0 before:right-0">
+      <div
+        class="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#da3a2a] before:top-0 before:right-0"
+      >
         <div class="relative z-20 pl-2 pr-12">
-          <h2 class="text-gray-100 font-black text-5xl leading-snug mb-10">Design <br>Philosophy</h2>
+          <h2 class="text-gray-100 font-black text-5xl leading-snug mb-10">
+            Design <br />Philosophy
+          </h2>
           <p class="text-gray-100 text-sm">
-            Inspired by smart aesthetics, HAVIT focuses on essential needs, blending omnisonic scenarios, comfort, style, and innovative technology to create visually appealing, user-centric products.
+            Inspired by smart aesthetics, HAVIT focuses on essential needs,
+            blending omnisonic scenarios, comfort, style, and innovative
+            technology to create visually appealing, user-centric products.
           </p>
 
           <p class="text-gray-100">
-            <span class="text-xl font-bold block mt-8">H655BT Hybrid ANC Wireless Headphones</span>
-            Utility Model Patents<br><br>
-            <span class="text-xl font-bold block mt-8">OPENBUDS 1 Open-Ear Clip Headphones</span>
-            iF Design Award<br>
-            MUSE Design Awards<br>
-            International Design Excellence Awards<br><br>
-            <span class="text-xl font-bold block mt-8">Freego1 Air Air Conduction Wireless Headphones</span>
+            <span class="text-xl font-bold block mt-8"
+              >H655BT Hybrid ANC Wireless Headphones</span
+            >
+            Utility Model Patents<br /><br />
+            <span class="text-xl font-bold block mt-8"
+              >OPENBUDS 1 Open-Ear Clip Headphones</span
+            >
+            iF Design Award<br />
+            MUSE Design Awards<br />
+            International Design Excellence Awards<br /><br />
+            <span class="text-xl font-bold block mt-8"
+              >Freego1 Air Air Conduction Wireless Headphones</span
+            >
             Canton Fair Design Award
           </p>
         </div>
       </div>
       <div class="w-full flex flex-col pl-16">
         <div class="h-full mt-auto overflow-hidden relative">
-          <img src="/img/products.webp" class="h-full w-full object-contain" alt="">
+          <img
+            src="/img/products.webp"
+            class="h-full w-full object-contain"
+            alt=""
+          />
         </div>
       </div>
-
     </div>
   </div>
 
+  <div class="relative overflow-hidden bg-white">
+    <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+      <div class="flex flex-col items-end pr-12 pt-8">
+        <div class="h-full mt-auto overflow-hidden relative">
+          <img src="/img/hq.jpg" alt="" />
+        </div>
+      </div>
+
+      <div
+        class="py-20 bg-gray-900 relative before:absolute before:h-full before:w-screen before:bg-gray-900 before:top-0 before:left-0"
+      >
+        <div class="relative z-20 pl-12">
+          <h2 class="text-[#da3a2a] font-black text-5xl leading-snug mb-10">
+            Global Tech Exhibitions
+          </h2>
+          <p class="text-white text-sm">
+            HAVIT showcases its leading consumer electronics at global tech
+            exhibitions, fueling industry growth through innovation. We aim to
+            deliver an exceptional smart living experience to all consumers.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
