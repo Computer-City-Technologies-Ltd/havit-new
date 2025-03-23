820d97bbd8970a218584ee0a18d5079d07c667d2
alin-comcity <1000957@daffodil.ac>
2025-03-23 15:27:56 +0600

    change sliders

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,41 +1,64 @@
 <template>
-    <div id="image" data-carousel='{ "loadingClasses": "opacity-0", "isAutoPlay": true, "isInfiniteLoop": true }' class="relative w-full">
+  <div
+    id="image"
+    data-carousel='{ "loadingClasses": "opacity-0", "isAutoPlay": true, "isInfiniteLoop": true }'
+    class="relative w-full"
+  >
     <div class="carousel rounded-none">
-        <div class="carousel-body h-full opacity-0">
+      <div class="carousel-body h-full opacity-0">
         <!-- Slide 1 -->
         <div class="carousel-slide">
-            <div class="flex h-full justify-center">
-            <img src="/img/slider-1.jpg" class="size-full object-cover" alt="game" />
-            </div>
+          <div class="flex h-full justify-center">
+            <img
+              src="/img/slider-1.webp"
+              class="size-full object-cover"
+              alt="game"
+            />
+          </div>
         </div>
         <!-- Slide 2 -->
         <div class="carousel-slide">
-            <div class="flex h-full justify-center">
-            <img src="/img/slider-2.jpg" class="size-full object-cover" alt="vrbox" />
-            </div>
+          <div class="flex h-full justify-center">
+            <img
+              src="/img/slider-2.webp"
+              class="size-full object-cover"
+              alt="vrbox"
+            />
+          </div>
         </div>
         <!-- Slide 3 -->
         <div class="carousel-slide">
-            <div class="flex h-full justify-center">
-            <img src="/img/slider-3.jpg" class="size-full object-cover" alt="laptop" />
-            </div>
+          <div class="flex h-full justify-center">
+            <img
+              src="/img/slider-3.webp"
+              class="size-full object-cover"
+              alt="laptop"
+            />
+          </div>
         </div>
-        </div>
+      </div>
     </div>
     <!-- Previous Slide -->
     <button type="button" class="carousel-prev">
-        <span class="size-9.5 bg-base-100 flex items-center justify-center rounded-full shadow">
-        <span class="icon-[tabler--chevron-left] size-5 cursor-pointer rtl:rotate-180"></span>
-        </span>
-        <span class="sr-only">Previous</span>
+      <span
+        class="size-9.5 bg-base-100 flex items-center justify-center rounded-full shadow"
+      >
+        <span
+          class="icon-[tabler--chevron-left] size-5 cursor-pointer rtl:rotate-180"
+        ></span>
+      </span>
+      <span class="sr-only">Previous</span>
     </button>
     <!-- Next Slide -->
     <button type="button" class="carousel-next">
-        <span class="sr-only">Next</span>
-        <span class="size-9.5 bg-base-100 flex items-center justify-center rounded-full shadow">
-        <span class="icon-[tabler--chevron-right] size-5 cursor-pointer rtl:rotate-180"></span>
-        </span>
+      <span class="sr-only">Next</span>
+      <span
+        class="size-9.5 bg-base-100 flex items-center justify-center rounded-full shadow"
+      >
+        <span
+          class="icon-[tabler--chevron-right] size-5 cursor-pointer rtl:rotate-180"
+        ></span>
+      </span>
     </button>
-    </div>
+  </div>
 </template>
-  
