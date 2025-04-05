1f162de1692d931cfffce909b9cc4c990c5c15ca
alin-comcity <1000957@daffodil.ac>
2025-04-05 14:08:09 +0600

    fix slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="relative h-screen overflow-hidden">
-    <img
-      src="/img/banner_3.webp"
-      class="absolute block w-full"
-      alt="banner_3"
-    />
+  <div class="overflow-hidden">
+    <img src="/img/banner_3.webp" class="w-full" alt="banner_3" />
   </div>
 </template>
